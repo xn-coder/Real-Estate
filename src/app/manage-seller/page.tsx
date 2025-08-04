bc7aaad790498f3d4812adad64a5911288a188e7
Xncoder <xncoder2003@gmail.com>
2025-08-04 15:44:16 +0000

    seller registration opens when click on add seller

--- src/app/manage-seller/page.tsx
+++ src/app/manage-seller/page.tsx
@@ -78,7 +78,7 @@ export default function ManageSellerDashboardPage() {
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold tracking-tight font-headline">Seller Management</h1>
         <Button asChild>
-          <Link href="/manage-seller/add">
+          <Link href="/register/seller" target="_blank">
             <UserPlus className="mr-2 h-4 w-4" /> Add Seller
           </Link>
         </Button>
